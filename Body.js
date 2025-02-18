94ef606bca7cc7cd0d5f61afb372bbb92cd2fccb
KirtiMhjn <kirtisunilmhjn@gmail.com>
2025-02-18 22:28:54 +0530

    assignment

--- Body.js
+++ Body.js
@@ -3,8 +3,8 @@ const Body =  () => {
     return  (
         <div className="Body">
             <div className="content">
-              <h1>Our Values</h1>
-              <h3>eTechno Lab is a Software Services company, provides IT solutions to <br /> our clients with our core values Collaborative spirit, Integrity, and <br /> Serenity.</h3>
+              <h1>eTechno Lab </h1>
+              <h3>eTechno Lab is a Software Services company, provides IT solutions to <br /> our clients with our core values Collaborative spirit, Integrity, and <br /> Serenity.<br />eTechno Lab is among one of the best application development companies <br /> in India, specializing in developing high-quality Web, Android, iOS <br />applications for clients across the globe.</h3>
             </div>
             
         </div>
